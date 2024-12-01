8dabbdb974f8f68742b442151e74717ea7e07c5f
kritagya.shakya <kritagya.shakya@f1soft.com>
2024-12-01 15:39:45 +0545

    Added: SchedularConfig and Schedular Job Factory

---
+++ src/main/java/com/kritagya/jobschedule/config/SchedulerConfig.java
@@ -0,0 +1,86 @@
+package com.kritagya.jobschedule.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.CronTrigger;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.SimpleTrigger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.*;
+
+import javax.sql.DataSource;
+import java.util.Date;
+import java.util.Properties;
+
+@Slf4j
+@Configuration
+public class SchedulerConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private QuartzProperties quartzProperties;
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerJobFactory jobFactory = new SchedulerJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties.getProperties());
+
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setOverwriteExistingJobs(true);
+        factory.setDataSource(dataSource);
+        factory.setQuartzProperties(properties);
+        factory.setJobFactory(jobFactory);
+        return factory;
+    }
+
+    public CronTrigger createCronTrigger(String cronExpression, Date startTime, String triggerName, int misFireInstruction) {
+        CronTriggerFactoryBean factoryBean = new CronTriggerFactoryBean();
+        factoryBean.setName(triggerName);
+        factoryBean.setStartTime(startTime);
+        factoryBean.setCronExpression(cronExpression);
+        factoryBean.setMisfireInstruction(misFireInstruction);
+        try{
+            factoryBean.afterPropertiesSet();
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+        }
+        return factoryBean.getObject();
+    }
+
+    public SimpleTrigger createSimpleTrigger(String triggerName, Date startTime, Long repeatTime, int misFireInstruction){
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setName(triggerName);
+        factoryBean.setStartTime(startTime);
+        factoryBean.setRepeatInterval(repeatTime);
+        factoryBean.setMisfireInstruction(misFireInstruction);
+        factoryBean.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
+        factoryBean.afterPropertiesSet();
+        return factoryBean.getObject();
+    }
+
+    public JobDetail createJob(Class<? extends QuartzJobBean> jobClass, boolean isDurable, ApplicationContext applicationContext, String jobName, String jobGroup ){
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(jobClass);
+        factoryBean.setDurability(isDurable);
+        factoryBean.setName(jobName);
+        factoryBean.setApplicationContext(applicationContext);
+        factoryBean.setGroup(jobGroup);
+
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put(jobName + jobGroup, jobClass.getName());
+        factoryBean.setJobDataMap(jobDataMap);
+        factoryBean.afterPropertiesSet();
+        return factoryBean.getObject();
+    }
+}
---
+++ src/test/java/com/kritagya/jobschedule/JobScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kritagya.jobschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
